85f2107cf4b071e184aa41997492ba46aa9de036
EC2 Default User <ec2-user@ip-172-31-81-137.ec2.internal>
2022-01-29 21:13:23 +0000

    my lab2

--- lab2.py
+++ lab2.py
@@ -1,51 +1,41 @@
-"""
-This is a regional comment
+'''
+lab 2 
+'''
 
-Lec2
+#3.1
+my_name = "Tom"
 
-"""
+print(my_name)
 
-#print('Hello World') #This is a comment
+#3.2
+my_id = 123
 
-#print( type( 123) )
+print(my_id)
 
-#print( type (123.) )
+#3.3
+#_123=my_id
+my_id = your_id = 123
 
-#print( type( '123') )
+print(my_id)
+print(your_id)
 
-#print("hello world" )
-#print('hello world')
+#3.4
+my_id_str = '123'
 
-#print( "Tom's cat" )
+print(my_id_str)
 
-#print('Hello World')
+#3.5
+#print(my_name + my_id)
 
-#print('Hello World'.upper()   )
+#3.6
+print(my_name + my_id_str)
 
-#print('Hello World'.lower())
+#3.7
+print(my_name * 3)
 
-#print('  Hello World  ')
-#print('  Hello World  '.strip())
+#3.8
+print('hello world. This is my first python string.'.split('.'))
 
-#print('Hello World'.split())
-#print('Hello, World'.split(','))
-
-#print('Hello, World'.split('o'))
-
-#print(2. ** 3)
-
-my_int = 123
-
-print(my_int)
-
-my_int = 321
-
-print(my_int)
-
-my_float = 123.
-
-my_str = 'hello world'
-
-print(my_float + 1)
-
-print(my_str.upper())
+#3.9
+message = "Tom's ID is 123"
+print(message)
